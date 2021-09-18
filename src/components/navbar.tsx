686033e8771761cb297d6f88181d506513d720da
Harpreet Singh <hsingh9600@gmail.com>
2021-09-19 05:44:02 +1200

    add flex styling and header footer

---
+++ src/components/navbar.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+const Navbar = () => {
+	return (
+		<div className="w-screen h-8 bg-blue-400">
+		</div>
+	)
+}
+
+export default Navbar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,38 @@
 import * as React from "react"
 import { StaticImage } from "gatsby-plugin-image"
+import Navbar from "../components/navbar"
 
 const HomePage = () => {
 	return (
-		<div className="flex justify-center items-center gap-4">
-			<div>
-				<StaticImage
-					// className="bio-avatar"
-					layout="fixed"
-					formats={["auto", "webp", "avif"]}
-					src="../images/avatar.png"
-					width={333}
-					height={399}
-					quality={95}
-					alt="Profile picture"
-				/>
+		<div className="flex flex-col justify-center h-screen justify-between">
+			<Navbar />
+			<div className="flex justify-center items-center gap-4">
+				<div>
+					<StaticImage
+						// className="bio-avatar"
+						layout="fixed"
+						formats={["auto", "webp", "avif"]}
+						src="../images/avatar.png"
+						width={333}
+						height={399}
+						quality={95}
+						alt="Profile picture"
+					/>
+				</div>
+				<div className="space-y-3 max-w-prose">
+					<p className="text-4xl font-sans font-semibold">Hey! I'm Harpreet Singh</p>
+					<p className="text-2xl font-sans font-medium text-justify">
+						I am a software engineer in Auckland, New Zealand.
+						Here, you can see the projects I work on and read my blogs which are mostly about tech.
+						I mostly do full stack web dev but I'm also really interested in computer architecture, network engineering,
+						machine learning and FPGAs.
+					</p>
+					<p className="text-2xl font-sans font-medium text-justify">
+						Hope you like my work! 😄
+					</p>
+				</div>
 			</div>
-			<div>
-				<p className="text-6xl font-sans font-bold">Harpreet Singh</p>
-				<p className="text-6xl font-mono font-black">Harpreet Singh</p>
+			<div className="w-screen h-5 bg-blue-400">
 			</div>
 		</div>
 	)
